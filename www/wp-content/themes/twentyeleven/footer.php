17e96802f6e49011f130acadac9e7fabafd94476
cube13 <cub4lt@gmail.com>
2019-01-11 16:22:37 +0200

    .

---
+++ www/wp-content/themes/twentyeleven/footer.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Template for displaying the footer
+ *
+ * Contains the closing of the id=main div and all content after
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+?>
+
+	</div><!-- #main -->
+
+	<footer id="colophon" role="contentinfo">
+
+			<?php
+				/*
+				 * A sidebar in the footer? Yep. You can customize
+				 * your footer with three columns of widgets.
+				 */
+				if ( ! is_404() )
+					get_sidebar( 'footer' );
+			?>
+
+			<div id="site-generator">
+				<?php do_action( 'twentyeleven_credits' ); ?>
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyeleven' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentyeleven' ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyeleven' ), 'WordPress' ); ?></a>
+			</div>
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-3446700-12', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+</body>
+</html>
